0c425fabb23fbc14007525e48e7ee7583a36044d
Mahmoud-Wafi <mahmoudwafi33@gmail.com>
2024-09-16 22:57:32 +0300

    models added

---
+++ tickets/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Movie (models.Model):
+    hall= models.CharField(max_length=20)
+    movie=models.CharField(max_length=20)
+    date=models.DateField()
+    
+    
+    def __str__(self) -> str:
+        return self.movie
+    
+class Guest(models.Model):
+    name= models.CharField(max_length=40 , null= False , blank=False)
+    phone_number=models.CharField(max_length=15)
+    
+        
+    def __str__(self) -> str:
+        return self.name
+    
+    
+class Reservation(models.Model):
+    guest= models.ForeignKey(Guest , related_name='reservation', on_delete=models.CASCADE)    
+    movie= models.ForeignKey(Movie , related_name='reservation', on_delete=models.CASCADE)    
+
+    
